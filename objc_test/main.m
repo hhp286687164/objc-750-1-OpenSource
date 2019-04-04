f454d857240efef9e338f14b5e71db6f64747689
韩红坡 <hanhongpo@corp.netease.com>
2019-04-04 19:38:42 +0800

    init

---
+++ objc_test/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  objc_test
+//
+//  Created by hanhongpo on 2019/4/4.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Class newClass = objc_allocateClassPair(objc_getClass("NSObject"), "newClass", 0);
+        objc_registerClassPair(newClass);
+        id newObject = [[newClass alloc]init];
+        NSLog(@"%@",newObject);
+    }
+    return 0;
+}
